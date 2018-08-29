8230a0fc4b5034cbc42785bcf28f87da2894f3d2
boys <474573550@qq.com>
2018-08-29 20:11:16 +0800

    添加了一些页面

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,24 @@ var uglify=require("gulp-uglify")
 
 
 //默认程序
-gulp.task("default",/*["compile"],*/function(){
+gulp.task("default",["compile"],function(){
 	gulp.src("src/scss/*.scss")
 	.pipe(sass())
 	.pipe(gulp.dest("css/"))
 	.pipe(livereload())
 })
-/*//编译es6
+//编译es6
 gulp.task("compile",function(){
-	gulp.src("src/script/*.js")
+	gulp.src("src/js/*.js")
 		.pipe(babel({
 			presets:["es2015"]
 		}))
 		//.pipe(uglify({ mangle: false }))
-		.pipe(gulp.dest("clubcmsscript/script"))
+		.pipe(gulp.dest("js/"))
 })
-*/
+
   
   
-gulp.watch(["src/scss/*.scss",/*"src/script/*.js"*/],["default"])
+gulp.watch(["src/scss/*.scss","src/js/*.js"],["default"])
 
 
---
+++ js/map.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var map = new BMap.Map("map");
+var point = new BMap.Point(116.404, 39.915);
+map.centerAndZoom(point, 15);
+window.setTimeout(function () {
+    map.panTo(new BMap.Point(116.409, 39.918));
+}, 2000);
