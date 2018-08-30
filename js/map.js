d0e561a8908020fe5d10a3175438bbd057789224
boys <474573550@qq.com>
2018-08-30 13:04:16 +0800

    更改字体大小和行距

--- js/map.js
+++ js/map.js
@@ -1,8 +1,29 @@
-"use strict";
+'use strict';
 
 var map = new BMap.Map("map");
 var point = new BMap.Point(116.404, 39.915);
 map.centerAndZoom(point, 15);
-window.setTimeout(function () {
-    map.panTo(new BMap.Point(116.409, 39.918));
-}, 2000);
+//开启鼠标滚轮缩放
+map.enableScrollWheelZoom(true);
+//平移缩放控件
+/*map.addControl(new BMap.NavigationControl())
+map.addControl(new BMap.ScaleControl())
+map.addControl(new BMap.OverviewMapControl())
+map.addControl(new BMap.MapTypeControl())
+*/
+var opts = {
+	offset: new BMap.Size(150, 5)
+};
+map.addControl(new BMap.ScaleControl(opts));
+
+var typeopts = {
+	type: 'MAP_NAVIGATION_CONTROL_ZOOM'
+};
+map.addControl(new BMap.NavigationControl(typeopts));
+
+function ZoomControl() {
+	this.defaultAnchor = 'BMAP_ANCHOR_TOP_LEFT';
+	this.defaultOffset = new BMap.Size(10, 10);
+}
+
+ZoomControl.prototype = new BMap.Control();
